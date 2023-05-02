057228e8368cc909db5d70b39bf1d0886ed0471d
yifeiche1121 <yifeiche@andrew.cmu.edu>
2023-05-02 14:37:27 -0400

    Update menu.js

--- FP/js/menu.js
+++ FP/js/menu.js
@@ -4,6 +4,8 @@ const params = new URLSearchParams(queryString);
 const poem = params.get('poem');
 var excerptEn = poems[poem]["excerpt-en"];
 var imageFile = poems[poem]["imgs"][0];
+var imageAlt = poems[poem]["imgs-alt"][0];
 document.getElementById("poem-img").src = "images/" + imageFile;
+document.getElementById("poem-img").alt = imageAlt;
 document.getElementById("exerpt-text").innerHTML = excerptEn;
 
